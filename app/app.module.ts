51382aa8d133bb176f567fe975dc0ac759858eb2
pankaj gupta <gupta89aman@gmail.com>
2018-02-26 23:08:37 +0530

    committing till chapter 3

---
+++ app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+
+@NgModule({
+ declarations: [EventsAppComponent,
+                EventsListComponent,
+                EventThumbnailComponent,
+                NavBarComponent           
+               ],
+ imports: [BrowserModule],
+ bootstrap: [EventsAppComponent]
+})
+export class AppModule
+{
+
+}
